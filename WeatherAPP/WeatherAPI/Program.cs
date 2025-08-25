8359cef1f0efce02b118a30e11e392c4314d8a20
SYLCN05 <Siyaryalcin25@gmail.com>
2025-08-25 21:32:37 +0200

    update program.cs
    added httpclient service

--- WeatherAPP/WeatherAPI/Program.cs
+++ WeatherAPP/WeatherAPI/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddHttpClient();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,7 +17,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
