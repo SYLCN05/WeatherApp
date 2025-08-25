feba262cac1c9a4f9d52aaa84ee1b30484fba465
SYLCN05 <Siyaryalcin25@gmail.com>
2025-08-25 22:34:33 +0200

    added a user secret for the api key
    its now possible to use enviorment variables through the secrets.json and secrets service i added.
    now the api key can be easily accessed through the static secrets class

---
+++ WeatherAPP/WeatherAPI/Services/Secrets.cs
@@ -0,0 +1,20 @@
+﻿namespace WeatherAPI.Services
+{
+    public static class Secrets
+    {
+
+        private static IConfiguration configuration;
+
+        static Secrets() 
+        {
+            configuration = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+        }
+
+        public static string OpenWeatherApiKey()
+        {
+          return  configuration["OpenWeather:API_KEY"] ?? throw new Exception("API key niet gevonden!");
+        }
+    }
+}
