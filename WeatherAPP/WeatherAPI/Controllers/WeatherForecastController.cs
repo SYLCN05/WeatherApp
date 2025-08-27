99a14f23097cd633605709411b3d690e12f4b0ad
SYLCN05 <Siyaryalcin25@gmail.com>
2025-08-27 20:30:04 +0200

    Update weatherforcast
    now the  postalcodetogeocode api endpoint calls the getcurrentdata endpoint, with the lon en lan as parameters. the get current data endpoint makes it possible to filter desired data from the request returning only the wanted information to make use of it.

--- WeatherAPP/WeatherAPI/Controllers/WeatherForecastController.cs
+++ WeatherAPP/WeatherAPI/Controllers/WeatherForecastController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Text.Json.Serialization;
 using Microsoft.Extensions.Options;
 using WeatherAPI.Services;
+using System.Text.Json;
 
 namespace WeatherAPI.Controllers
 {
@@ -20,15 +21,21 @@ namespace WeatherAPI.Controllers
         [HttpGet("{Zipcode}")]
         public async Task<IActionResult> PostalcodeToGeocode([FromRoute] string Zipcode,[FromQuery] string CountryCode)
         {
-            _httpClient.BaseAddress = new Uri("http://api.openweathermap.org/geo/1.0/");
+          
             _httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
 
-            var response = await _httpClient.GetAsync($"zip?zip={Zipcode},{CountryCode}&appid={API_KEY}");
+            var response = await _httpClient.GetAsync($"http://api.openweathermap.org/geo/1.0/zip?zip={Zipcode},{CountryCode}&appid={API_KEY}");
 
             if (response.IsSuccessStatusCode)
             {
                 var result = await response.Content.ReadAsStringAsync();
-                return Ok(result);
+                using JsonDocument doc = JsonDocument.Parse(result);
+
+                var lat = doc.RootElement.GetProperty("lat").GetDouble();
+                var lon = doc.RootElement.GetProperty("lon").GetDouble();
+                var current = await GetCurrentData(lat, lon);
+
+                return Ok(current);
             }
             return BadRequest();
         }
@@ -36,15 +43,23 @@ namespace WeatherAPI.Controllers
         [HttpGet]
         public async Task<IActionResult> GetCurrentData([FromQuery] double lat, [FromQuery] double lon)
         {
-            _httpClient.BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/");
-         
 
-            var response = await _httpClient.GetAsync($"weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric");
+            var response = await _httpClient.GetAsync($"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric");
 
             if (response.IsSuccessStatusCode) 
             {
                 var result = await response.Content.ReadAsStringAsync();
-                return Ok(result);
+                using var doc = JsonDocument.Parse(result);
+
+                var formatted = new
+                {
+                    Stad = doc.RootElement.GetProperty("name").GetString(),
+                    Temp = doc.RootElement.GetProperty("main").GetProperty("temp").GetDouble(),
+                    mintemp = doc.RootElement.GetProperty("main").GetProperty("temp_min").GetDouble()
+                };
+
+                return Ok(formatted);
+
             }
             return BadRequest();
         }
